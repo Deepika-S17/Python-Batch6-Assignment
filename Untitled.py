9a6d9529bbef0c363be099a2961332fd0f934076
Deepika S <68246385+Deepika-S17@users.noreply.github.com>
2020-07-23 02:47:23 +0530

    Batch 6 Day 2 Assign

---
+++ Untitled.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello ")
+
+
+# In[15]:
+
+
+print ("hlopyt")
+
+
+# In[16]:
+
+
+print("hlo")
+
+
+# In[45]:
+
+
+print("deee\n san")
+
+
+# In[44]:
+
+
+name="deepika"
+age=21
+marks=91.56
+print(f"the name is {name} age is {age} marks is {marks}")
+
+
+# In[27]:
+
+
+a = 60;b = 13;print(a,b);
+y=bin(a);print(y);
+y=bin(a)[2:];print(y)
+z=bin(b)[2:];print(z)
+
+#a&b = 0000 1100
+print(bin(a&b)[2:])
+#a|b = 0011 1101
+print(bin(a|b)[2:])
+#a^b = 0011 0001
+print(bin(a^b)[2:])
+#~a = 1100 0011
+print(bin(~a)[3:])
+#a>>2
+print(bin(a>>2)[2:])
+#b<<3
+print(bin(b<<3)[2:])
+
+
+# In[43]:
+
+
+s="iloveindia"
+"g"  not in s
+
+
+# In[32]:
+
+
+a=10
+b=10
+a==b
+
+
+# In[33]:
+
+
+a is b
+
+
+# In[34]:
+
+
+id(a)
+
+
+# In[36]:
+
+
+id (b)
+
+
+# In[37]:
+
+
+x=1.5
+y=1.5
+x==y
+
+
+# In[39]:
+
+
+x is y
+
+
+# In[40]:
+
+
+x is y
+
+
+# In[41]:
+
+
+a=257
+b=257
+a==b
+
+
+# In[42]:
+
+
+a is b
+
+
+# In[ ]:
+
+
+
+
